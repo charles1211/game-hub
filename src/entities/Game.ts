3d9462ef54c3c0b9151b2b80529efc12f833cde2
Charles <Charlescabarrus1211@gmail.com>
2023-05-15 10:17:35 +0800

    Build game attributes

--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
+import { Genre } from './Genre';
+import { Publisher } from './Publisher';
 import { platForm } from './platForm';
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: platForm }[];
