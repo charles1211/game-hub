721a5c0d91ba3dc4d01860401672b859b76ca85a
Charles <Charlescabarrus1211@gmail.com>
2023-05-15 11:15:21 +0800

    Improve game details layout

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,19 +12,21 @@ interface GameCardProps {
 
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent='space-between' marginBottom={3}>
-          <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize={'2xl'}>
-          <Link to={'/games/' + game.slug}> {game.name}</Link>
-          <Emoji rating={game.rating_top} />{' '}
-        </Heading>
-      </CardBody>
-    </Card>
+    <Link to={'/games/' + game.slug}>
+      <Card>
+        <Image src={getCroppedImageUrl(game.background_image)} />
+        <CardBody>
+          <HStack justifyContent='space-between' marginBottom={3}>
+            <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+          <Heading fontSize={'2xl'}>
+            {game.name}
+            <Emoji rating={game.rating_top} />{' '}
+          </Heading>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
